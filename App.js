a280e1e3fa575ccfa759786baa397c506fe42685
samyrd309 <samyrd309@gmail.com>
2022-10-26 17:45:19 -0400

    File format change

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import Register from './components/Register.js';
 import ScanQR from './components/ScannerQR.js';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './components/Home.js';
 
 export default function App() {
 
@@ -14,6 +15,7 @@ export default function App() {
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Register" component={Register} />
         <Stack.Screen name="ScanQR" component={ScanQR} />
+        <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
